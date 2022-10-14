eeca56d317694fdcaeb4e055a028a9c72b03b46b
Guilherme Henrique <108905023+oisouguilherme@users.noreply.github.com>
2022-10-14 17:30:51 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css';
+import { SignIn } from './pages/SignIn';
+
+export function App() {
+
+  return (
+    <SignIn />
+  )
+}
+
---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { Slot } from '@radix-ui/react-slot';
+
+import { clsx } from 'clsx';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode;
+    asChild?: boolean;
+}
+
+export function Button ({ children, asChild, className, ...props}: ButtonProps) {
+    const Comp = asChild ? Slot : 'button';
+
+    return (
+        <Comp
+        className={clsx (
+            'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-white', className,
+        )}
+        {...props}
+        >
+            {children}
+        </Comp>
+    )
+}
